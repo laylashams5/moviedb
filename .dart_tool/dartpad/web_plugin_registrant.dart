4217e7ab8de18028d9b336c9d144ed6aefa59cbb
laylashams5 <laylashams5@gmail.com>
2023-12-21 19:47:38 +0400

    ...

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -8,11 +8,13 @@
 
 import 'package:connectivity_plus/src/connectivity_plus_web.dart';
 import 'package:shared_preferences_web/shared_preferences_web.dart';
+import 'package:video_player_web/video_player_web.dart';
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 void registerPlugins([final Registrar? pluginRegistrar]) {
   final Registrar registrar = pluginRegistrar ?? webPluginRegistrar;
   ConnectivityPlusWebPlugin.registerWith(registrar);
   SharedPreferencesPlugin.registerWith(registrar);
+  VideoPlayerPlugin.registerWith(registrar);
   registrar.registerMessageHandler();
 }
