1d41c35920c8b0bf3717dcb6750ab73634273028
laylashams5 <laylashams5@gmail.com>
2023-12-25 12:26:25 +0400

    remove movies list

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -7,12 +7,6 @@ const routes: Routes = [
     path: '',
     component: HomeComponent,
   },
-
-  {
-    path: 'movies',
-    loadChildren: () => import('./components/movies/movies.module').then(mod => mod.MoviesModule)
-  },
-
   {
     path: 'movies/:id', loadChildren: () => import('./components/movie-details/movie-details.module').then(mod => mod.MovieDetailsModule)
   },
